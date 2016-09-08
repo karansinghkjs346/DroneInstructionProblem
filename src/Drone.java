8e28a198657337d9f150f3be82f2e9655bf1539f
karansinghkjs346 <karansinghkjs346@gmail.com>
2016-09-09 04:24:30 +0530

    Update Drone.java

--- src/Drone.java
+++ src/Drone.java
@@ -1,5 +1,5 @@
 //Drone class is used to maintin the coordinates , directiona and if the drone has started or not 
-//With the help of this class more drones can be added and handled 
+//With the help of this class more drones can be added and handeled 
 public class Drone {
 
 	int x, y;
